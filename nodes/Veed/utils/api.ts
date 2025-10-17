44b680d835e6debdc37678f42402b739dd38e30f
Dmytro Baltak <dmytro.baltak@veed.io>
2025-10-17 18:22:25 +0200

    fix: use generic eslint-disable-next-line comment

--- nodes/Veed/utils/api.ts
+++ nodes/Veed/utils/api.ts
@@ -11,7 +11,7 @@ const FAL_QUEUE_BASE_URL = 'https://queue.fal.run';
  * Sleep utility for polling delays
  * as per Airtop official example
  */
-// eslint-disable-next-line @n8n/community-nodes/no-restricted-globals
+// eslint-disable-next-line
 const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
 
 /**
